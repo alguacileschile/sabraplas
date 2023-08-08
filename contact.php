9907798cb6cb0c6ecc66b3c43d3fdbea7f41d6c6
Alguaciles <alguacileschile@gmail.com>
2023-08-08 10:58:46 -0400

    contact

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@
         }
 
         // FIXME: Update this to your desired email address.
-        $recipient = "yourgmail@gmail.com";
+        $recipient = "javieriuta@gmail.com";
 
         // Build the email content.
         $email_content = "Full Name: $name\n";
